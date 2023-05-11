98187a53d9806243f2b335690955b407b88ceba4
Saleh <saleh_jamali@ymail.com>
2023-05-11 03:44:56 +0330

    now run_server.sh handles the ip address and its trailing space

--- run_server.sh
+++ run_server.sh
@@ -1,2 +1,5 @@
 echo "$1"
-BOKEH_RESOURCES=inline bokeh serve $1 --allow-websocket-origin=192.168.100.100:5006
+myip="$(hostname -I)"
+myip_nospace=$(echo $myip | tr -d ' ')
+echo "IP: ${myip_nospace}"
+BOKEH_RESOURCES=inline bokeh serve $1 --allow-websocket-origin=${myip_nospace}:5006
